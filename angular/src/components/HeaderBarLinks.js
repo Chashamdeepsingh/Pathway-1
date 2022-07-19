22669344e1ce86d18fdf1c81023da5c97b99a7bd
Chashamdeepsingh <87883791+Chashamdeepsingh@users.noreply.github.com>
2022-07-19 17:16:58 +0300

    Add files via upload

---
+++ angular/src/components/HeaderBarLinks.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const HeaderBarLinks = () => (
+  <div className="navbar-menu">
+    <div className="navbar-end">
+      <div className="navbar-item">
+        <div className="buttons">
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-github fa-2x" aria-hidden="true" />
+          </a>
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-twitter fa-2x" aria-hidden="true" />
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HeaderBarLinks;
